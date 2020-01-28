838a735bc23183e1fdca44ede02f0710e6600961
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-01-28 15:00:24 -0600

    added script for env vars

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 const fs = require('fs')
 
-fs.writeFileSync('./.env', `REACT_APP_API_KEY=${process.env.REACT_APP_API_KEY}\n `)
+fs.writeFileSync('./.env', `REACT_APP_API_KEY=${process.env.REACT_APP_API_KEY}\n 
+                            REACT_APP_ADMIN_KEY=${process.env.REACT_APP_ADMIN_KEY}\n
+                            REACT_APP_USER=${process.env.REACT_APP_USER}\n
+                            REACT_APP_UNDERGROUND_API_KEY=${process.env.REACT_APP_UNDERGROUND_API_KEY}\n
+                            REACT_APP_POST_API_KEY=${process.env.REACT_APP_POST_API_KEY}\n
+                            REACT_APP_LOGIN_API_KEY=${process.env.REACT_APP_API_KEY}\n
+                            REACT_APP_REGISTER_API_KEY=${process.env.REACT_APP_REGISTER_API_KEY}\n
+                            REACT_APP_DELETE_API_KEY=${process.env.REACT_APP_DELETE_API_KEY}\n
+                            REACT_APP_USERPOST_API_KEY=${process.env.REACT_APP_USERPOST_API_KEY}\n
+                            REACT_APP_USER_API_KEY=${process.env.REACT_APP_USER_API_KEY}\n
+                            REACT_APP_WEB_USER_API_KEY=${process.env.REACT_APP_WEB_USER_API_KEY}\n
+                            REACT_APP_USER_FAVS_API_KEY=${process.env.REACT_APP_USER_FAVS_API_KEY}\n
+                            REACT_APP_LINK_PREVIEW=${process.env.REACT_APP_LINK_PREVIEW}\n
+                `)
