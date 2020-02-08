a099a07126a6e211fa71a0570da64be9356ceb23
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-08 16:34:33 -0600

    created contact page and added boilerplate

---
+++ src/components/contact/contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class Contact extends React.Component{
+   
+ 
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return(
+            <div>
+            </div> 
+        )
+    }
+}
