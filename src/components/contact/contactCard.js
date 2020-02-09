285f6f22a53b5794436c83afba8d2a04a9e700c8
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-09 03:05:23 -0600

    added functionality for card slider

--- src/components/contact/contactCard.js
+++ src/components/contact/contactCard.js
@@ -3,14 +3,33 @@ import React from 'react'
 export default class ContactCard extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            messageOpen: false
+        }
+    }
+
+    expandHandler = () => {
+        if(this.state.messageOpen === false){
+            this.setState({
+                messageOpen: true 
+            })
+        } else if (this.state.messageOpen === true){
+            this.setState({
+                messageOpen: false
+            })
+        }
     }
 
     render(){
         return(
-            <div>
-                {this.props.message.name}
-                {this.props.message.email}
-                {this.props.message.message}
+            <div onClick={this.expandHandler}>
+                <p>{this.props.message.name}</p> 
+                {this.state.messageOpen === false ? '' : 
+                    <div>
+                        <p>{this.props.message.email}</p>
+                        <p>{this.props.message.message}</p> 
+                    </div>                       
+                }               
             </div> 
         )
     }
