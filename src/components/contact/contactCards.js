1efe5cef27883439f8fc658f8b18ef1e3ebba15a
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-09 02:16:51 -0600

    added boilerplate to contact cards

---
+++ src/components/contact/contactCards.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import axios from 'axios'
+
+
+export default class ContactCards extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            contactInfo: []
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    renderInfo = () => {
+
+    }
+
+    render(){
+        return(
+            <div>
+               Hello
+            </div> 
+        )
+    }
+}
