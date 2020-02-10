0c96e83e7a35c600d6e7bc2ed146fb0191ffc089
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-10 00:12:21 -0600

    finished contact view card for the admin

--- src/components/contact/contactCard.js
+++ src/components/contact/contactCard.js
@@ -33,20 +33,26 @@ export default class ContactCard extends React.Component{
             })
     }
 
+    trimTime = (myDate) => {
+       myDate = myDate.split('')
+       myDate.splice(-2, 2)
+       return myDate
+       }
+    
+
+
     render(){
         return(
             <div className='contact-card-container'> 
-                <div className='message-info'> 
-                    <div style={{width: '80%'}} onClick={this.expandHandler}> 
-                        <p>{this.props.message.date}</p> 
-                        <p>{this.props.message.name}</p>
-                        <p>{this.props.message.email}</p> 
-                    </div>
-                     <p onClick={() => {this.deleteHandler(this.props.message.id)}} className='contact-delete-button'>Delete</p> 
+                <div className='message-info' >                    
+                    <p>{this.trimTime(this.props.message.date)}</p> 
+                    <p>{this.props.message.name}</p>     
+                    <p onClick={this.expandHandler}> + </p>             
                 </div> 
                 <div className={this.state.messageOpen === true ? 'message-open' : 'message-closed'}>
-                    <p>Email: {this.props.message.email}</p>
+                    <p>{this.props.message.email}</p>
                     <p>{this.props.message.message}</p> 
+                    <p onClick={() => {this.deleteHandler(this.props.message.id)}} className='contact-delete-button'>Delete</p> 
                 </div>                                               
             </div> 
         )
